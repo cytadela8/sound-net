4f20a204b0caa4f2c8e6bab36eb6d2914ad6f706
Artur Puzio <contact@puzio.waw.pl>
2022-08-21 18:52:36 +0200

    Sending in packets

--- router/abstract_modem.py
+++ router/abstract_modem.py
@@ -38,20 +38,20 @@ class FloatLoop:
         return chunk
 
     def write(self, s):
-        while self.SIZE == self.available:
-            # print("Waiting ", len(s), self.available)
-            with self.notify:
-                self.notify.wait()
-        with self.lock:
-            towrite = min(self.SIZE - self.woffset, self.SIZE - self.available,
-                          len(s))
-            self.buffer[self.woffset:self.woffset + towrite] = s[:towrite]
-            self.woffset += towrite
-            self.available += towrite
-            if self.woffset == self.SIZE:
-                self.woffset = 0
-        if towrite < len(s):
-            self.write(s[towrite:])
+        while len(s) > 0:
+            while self.SIZE == self.available:
+                # print("Waiting ", len(s), self.available)
+                with self.notify:
+                    self.notify.wait()
+            with self.lock:
+                towrite = min(self.SIZE - self.woffset, self.SIZE - self.available,
+                              len(s))
+                self.buffer[self.woffset:self.woffset + towrite] = s[:towrite]
+                self.woffset += towrite
+                self.available += towrite
+                if self.woffset == self.SIZE:
+                    self.woffset = 0
+            s = s[towrite:]
 
 
 class Audio:
@@ -65,7 +65,8 @@ class Audio:
         self.buffer = np.array([], dtype=np.float32)
         self.prev_chunk = None
 
-        def callback(in_data: np.ndarray, out_data: np.ndarray, frames: int, time, status):
+        def callback(in_data: np.ndarray, out_data: np.ndarray, frames: int,
+                     time, status):
             # using Numpy to convert to array for processing
             # audio_data = np.fromstring(in_data, dtype=np.float32)
             out_data.fill(0)
@@ -73,7 +74,7 @@ class Audio:
             channel = np.append(channel, np.zeros(frames - len(channel),
                                                   dtype=np.float32))
             out_data[:, 0] = channel
-            self.buffer = np.append(self.buffer, in_data[:,0])
+            self.buffer = np.append(self.buffer, in_data[:, 0])
             while len(self.buffer) > chunk_size:
                 chunk = self.buffer[:chunk_size]
                 self.buffer = self.buffer[chunk_size:]
@@ -100,7 +101,7 @@ class Audio:
         self.handlers.append(handler)
 
 
-class AbstractModemSender(abc.ABC):
+class AbstractModem(abc.ABC):
     """Implements conversion from data to signal"""
 
     @abc.abstractmethod
@@ -113,45 +114,94 @@ class AbstractModemSender(abc.ABC):
         self._buffer = Queue(buf_size)
         self._player = Audio(chunk_size)
 
-    def write_bytes(self, data: bytes):
+    def write_bytes(self, data: bytes|list[int]):
         """
         Writes data to send. If buffer is full will block until
         all data is in buffer.
         """
-        for b in data:
-            self._buffer.put(b)
+        self._buffer.put([data])
 
-    def _consume_buffer(self, wait_nonempty=False) -> bytes:
-        """
-        Returns the content of buffer as bytes. Consumes returned content.
-        """
-        data = list()
-        if wait_nonempty:
-            data.append(self._buffer.get())
-        for _ in range(
-                self._buf_size if not wait_nonempty else self._buf_size - 1):
-            if self._buffer.empty():
-                break
-            data.append(self._buffer.get())
-        return bytes(data)
-
-    def flush(self, wait_nonempty=False):
+    def flush(self):
         """
         Empties the buffer. Should not be used from two threads at once!
         """
-        data = self._consume_buffer(wait_nonempty=wait_nonempty)
-        print("SENDING", data.hex())
+        [data] = self._buffer.get()
+        print("SENDING", data)
         signal = self.bytes_to_signal(data)
         self._player.play(signal)
 
     def start(self):
         while True:
             # Without wait_nonempty this would be a busyloop
-            self.flush(wait_nonempty=True)
+            self.flush()
 
 
-class TrivialModemSender(AbstractModemSender):
+class Encoder(abc.ABC):
+    @abc.abstractmethod
+    def encode(self, data):
+        """gets a list of some data, returns encoded list"""
+
+    @abc.abstractmethod
+    def decode(self, data):
+        """gets a list of encoded data and returns pair (decoded,
+        corrected_error_count) """
+
+
+class StackedModem(AbstractModem):
+    out = []
+    packet_buffer = []
+    IN_PACKET = "in"
+    NO_PACKET = "out"
+
+    def __init__(self, encoders: [Encoder], buf_size=1024, chunk_size=4410):
+        super().__init__(buf_size, chunk_size)
+
+        self._player.register_handler(self.handle_frame)
+        self.encoders = encoders
+        self.handlers = []
+
+    @abc.abstractmethod
+    def data_to_signal(self, data) -> np.ndarray:
+        """Converts encoded data into the sound wave"""
+
+    @abc.abstractmethod
+    def in_frame_decoder(self, frame: np.ndarray, prev_frame: np.ndarray):
+        """"""
+
+    def bytes_to_signal(self, data: bytes) -> np.ndarray:
+        for encoder in self.encoders:
+            data = encoder.encode(data)
+        return self.data_to_signal(data)
+
+    def handle_frame(self, frame: np.ndarray, prev_frame: np.ndarray):
+        if prev_frame is None:
+            return []
+        decoded, status = self.in_frame_decoder(frame, prev_frame)
+        if status == self.NO_PACKET:
+            packet = self.packet_buffer
+            self.packet_buffer = []
+            if len(packet) != 0:
+                print("RECEIVED", packet)
+
+            for i, encoder in enumerate(reversed(self.encoders)):
+                if len(packet) == 0:
+                    return packet
+                packet, errors = encoder.decode(packet)
+                print(f"Decoded stage={i} (error={errors}), data {packet}")
+            for handler in self.handlers:
+                handler(packet)
+        else:
+            if len(self.packet_buffer) == 0:
+                print("Receiving", flush=True, end="")
+            else:
+                print(".", flush=True, end="")
+            self.packet_buffer += decoded
+
+    def register_handler(self, handler):
+        self.handlers.append(handler)
+
+
+class TrivialModemSender(AbstractModem):
 
     def bytes_to_signal(self, data: bytes) -> np.ndarray:
         return np.array(data)
-
--- router/artur_modem.py
+++ router/artur_modem.py
@@ -4,26 +4,27 @@ import hamming_codec
 import numpy as np
 import time
 
-from abstract_modem import AbstractModemSender
+from abstract_modem import Encoder, StackedModem
+import logging
+
+logging.basicConfig(level=logging.INFO)
 
 
-class NaiveModemSender(AbstractModemSender):
+class NaiveModemSender(StackedModem):
     MIN_HZ = 2000
     MUL = (2.0 ** (1 / 6))
     CLK_NUM_LOW = 12
     CLK_NUM_HIGH = CLK_NUM_LOW + 1
     SYNC_DIV = 10
 
-    def __init__(self, buf_size=1024, chunk_size=4410):
-        super().__init__(buf_size, chunk_size)
+    def __init__(self, encoders, buf_size=1024, chunk_size=4410):
+        super().__init__(encoders, buf_size, chunk_size)
         self.sin_range = np.linspace(0, 2 * np.pi, self.chunk_size)
         self.min_cycles = self.MIN_HZ // (44100 / chunk_size)
         self.sync_chunk = chunk_size // self.SYNC_DIV
-        print("sync_chunk", self.sync_chunk)
-        self._player.register_handler(self.in_frame_handler)
-        print("waves base-hz",
-              [self.cycles_for_num(i) * (44100 // chunk_size) for i in
-               range(14)])
+        logging.info(f"sync_chunk {self.sync_chunk}")
+        logging.info(
+            f"waves base-hz {[self.cycles_for_num(i) * (44100 // chunk_size) for i in range(14)]}")
 
     def wave_by_hz(self, cycles: int):
         return np.sin(self.sin_range * cycles)
@@ -35,17 +36,17 @@ class NaiveModemSender(AbstractModemSender):
         base_cycles = self.cycles_for_num(num)
         tau = 0.7
         num = 1
-        return np.sum([self.wave_by_hz(base_cycles)]+
-            [self.wave_by_hz(base_cycles * (3+i)) * (tau ** i) for i in
-             range(1,num)], axis=0)
+        return np.sum([self.wave_by_hz(base_cycles)] +
+                      [self.wave_by_hz(base_cycles * (3 + i)) * (tau ** i) for i
+                       in
+                       range(1, num)], axis=0)
 
-    def bytes_to_signal(self, data: bytes) -> np.ndarray:
+    def data_to_signal(self, data: [int]) -> np.ndarray:
         # Use float32 because default is float64 and you will get garbage :P
         result = np.zeros(self.chunk_size * len(data), dtype=np.float32)
         for i, el in enumerate(data):
-            enc = int(hamming_codec.encode(el, 8), 2)
             wave = np.sum([self.wave_by_num(b) for b in range(12) if
-                           (enc & (1 << b)) != 0] + [
+                           (el & (1 << b)) != 0] + [
                               self.wave_by_num(self.CLK_NUM_LOW + (i % 2))],
                           axis=0)
             result[
@@ -53,14 +54,13 @@ class NaiveModemSender(AbstractModemSender):
                 np.abs(wave))
         return result
 
-    def in_frame_handler(self, frame: np.ndarray, prev_frame: np.ndarray):
-        if prev_frame is None:
-            return
+    def in_frame_decoder(self, frame: np.ndarray, prev_frame: np.ndarray):
         two_frame = np.append(prev_frame, frame)
         clk_diff = []
         subframes = two_frame.shape[0] // self.sync_chunk
-        summary = ""
-        for offset in range(0, two_frame.shape[0], self.sync_chunk):
+        summary = []
+        for offset in range(0, two_frame.shape[0] - self.sync_chunk + 1,
+                            self.sync_chunk):
             small_fft = np.abs(
                 np.fft.rfft(two_frame[offset:offset + self.sync_chunk]))
             low_power = small_fft[
@@ -68,17 +68,25 @@ class NaiveModemSender(AbstractModemSender):
             high_power = small_fft[
                 self.cycles_for_num(self.CLK_NUM_HIGH) // self.SYNC_DIV]
             clk_diff.append(low_power - high_power)
-            summary += "1" if low_power - high_power > 0 else "0"
+            summary.append(1 if low_power - high_power > 0 else -1)
+        finder_row = [1] * (subframes // 2) + [-1] * (subframes // 2)
+
+        signal_check = np.array(
+            [np.roll(finder_row, off) for off in range(subframes)],
+            dtype=np.float32).transpose()
+
         finder_row = [1] * (subframes // 2) + [-1] * (subframes // 2)
         finder = np.array(
             [np.roll(finder_row, off) for off in range(subframes // 2)],
             dtype=np.float32).transpose()
         offset_qual = np.abs(np.matmul(clk_diff, finder))
         signal_lvl = np.max(offset_qual)
-        print("SIG LEVEL", signal_lvl)
+        signal_check_val = np.max(np.matmul(summary, signal_check))
+        logging.debug(f"SIG LEVEL {signal_lvl} {signal_check_val}")
+        if signal_lvl < 2 or signal_check_val < self.SYNC_DIV * 1.7:
+            return [], self.NO_PACKET
 
         offset = np.argmax(offset_qual)
-        print(summary, offset)
         frame = two_frame[
                 self.sync_chunk * offset:self.sync_chunk * offset + self.chunk_size]
         ft = np.abs(np.fft.rfft(frame))
@@ -93,24 +101,89 @@ class NaiveModemSender(AbstractModemSender):
         for i in range(12):
             if present_waves[i]:
                 val |= (1 << i)
-        print(int(hamming_codec.decode(val, 12), 2), "from", val, "=",
-              "".join(map((lambda x: "1" if x else "0"), present_waves))[::-1])
+        return [val], self.IN_PACKET
+
+
+class HammingByte(Encoder):
+    def __init__(self):
+        self.decodeMap = {}
+        for i in range(1 << 12):
+            decoded = int(hamming_codec.decode(i, 12), 2)
+            self.decodeMap[i] = decoded, (
+                    i != int(hamming_codec.encode(decoded, 8), 2))
+
+    def encode(self, data):
+        return [int(hamming_codec.encode(el, 8), 2) for el in data]
+
+    def decode(self, data):
+        decoded = []
+        errors = 0
+        for el in data:
+            dec, error = self.decodeMap[el]
+            decoded.append(dec)
+            if error:
+                errors += 1
+
+            logging.debug(
+                f"""{dec} from {el}={"".join([str(el // (1 << i) % 2) for i in range(12)])[
+                                     ::-1]}, hasError={error}""")
+
+        return decoded, errors
+
+
+class PacketBorder:
+    SIZE = 3
+
+    def encode(self, data):
+        return [i for i in range(self.SIZE, 0, -1)] + data + [i for i in
+                                                              range(1,
+                                                                    self.SIZE + 1)]
+
+    def decode(self, data):
+        if data[0] > self.SIZE or data[0] == 0:
+            logging.warning(
+                f"INVALID PACKET. PREAMBULE NOT FOUND {len(data)} {data[:5]}")
+            return [], 1
+        if data[-1] > self.SIZE or data[-1] == 0:
+            logging.warning(
+                f"INVALID PACKET. POSTAMBULE NOT FOUND {len(data)} {data[-5:]}")
+            return [], 1
+        return data[data[0]:-data[-1]], 0
 
 
 if __name__ == '__main__':
-    modem = NaiveModemSender(10)
+    modem = NaiveModemSender([PacketBorder(), HammingByte()], 100, 441)
     modem_t = threading.Thread(target=modem.start)
     modem_t.start()
 
+    payload = [i for i in range(1 << 8)]
+
 
     def _writer():
-        for i in range(256):
-            modem.write_bytes(bytes([i for _ in range(1)]))
+        def handler(received):
+            if len(payload) != len(received):
+                logging.warning(
+                    f"different length! {len(payload)} {len(received)} {payload} {received}")
+            differences = []
+            for i, el_payload, el_received in zip(range(len(payload)), payload,
+                                                  received):
+                if el_payload != el_received:
+                    differences.append((i, el_payload, el_received))
+            print(f"Differences {differences}")
+
+        modem.register_handler(handler)
+        for i in range(5):
+            modem.write_bytes(payload)
 
 
-    time.sleep(2)
+    time.sleep(1)
     writer = threading.Thread(target=_writer)
     writer.start()
 
-    writer.join()
-    modem_t.join()
+    try:
+        writer.join()
+        modem_t.join()
+    except KeyboardInterrupt:
+        pass
+    finally:
+        print(modem.out)
